9313c3d334272a4ce7fa504d773291e61fac1d0a
João Roblez <gustavodeassis38@gmail.com>
2026-02-13 11:19:41 -0300

    up
    criação de chave aleatoria nao repetivel alfanumerica funcionando

--- FirebaseMultiplayer/Assets/Scripts/ControleRemoto/User.cs
+++ FirebaseMultiplayer/Assets/Scripts/ControleRemoto/User.cs
@@ -20,17 +20,21 @@ namespace ControleRemoto
         private string RA = "";
         private  string name = "";
         private PermissionsUsers authorityLevel = PermissionsUsers.NoAcess;
+        private char[] aleatorio = new[] {'A','B','C','D','E','F','G','H','I','J','K','L','M',
+            'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 
+            'a','b','c','d','e','f','g','h','i','j','k','l','m',
+            'n','o','p','q','r','s','t','u','v','w','x','y','z',
 
-
-        public string[] keys = {"Victor1234567890", "Gustavo1234567890", "Matheus1234567890", "Lucas1234567890", "Bruno1234567890", "Rafael1234567890", "Felipe1234567890", "Gabriel1234567890", "Enzo1234567890", "Leonardo1234567890"};
+            '0','1','2','3','4','5','6','7','8','9'}; // caracteres aleatorios
+        private Unity.Mathematics.Random  r = new Unity.Mathematics.Random();
 
         private void Start()
         {
-            ToKeyUser(keys);
+            
         }
 
-        public User(string id, string[] id_sala, string nome, Estado estado, float pooling, PermissionsUsers permissions)
+        public User(string id, string[] id_sala, string nome, Estado estado, float pooling, PermissionsUsers permissions, string[] keyUsers)
         {
             this.id = id;
             this.id_sala = id_sala;
@@ -38,8 +42,9 @@ namespace ControleRemoto
             this.estado = estado;
             this.pooling = pooling;
             this.permissions = permissions;
+            this.keyUser = ToKeyUser(keyUsers);
         }
-        public User(string id, string[] id_sala, string nome, Estado estado, PermissionsUsers permissions)
+        public User(string id, string[] id_sala, string nome, Estado estado, PermissionsUsers permissions, string[] keyUsers)
         {
             this.id = id;
             this.id_sala = id_sala;
@@ -47,6 +52,7 @@ namespace ControleRemoto
             this.estado = estado;
             this.pooling = 0;
             this.permissions = permissions;
+            this.keyUser = ToKeyUser(keyUsers);
         }
         public User()
         {
@@ -56,18 +62,20 @@ namespace ControleRemoto
             this.estado = Estado.Undefined;
             this.pooling = 0;
             this.permissions = PermissionsUsers.NoAcess;
+            this.keyUser = "null";
         }
         
-        public User(string id, string[] id_sala, string nome, float pooling)
+        public User(string id, string[] id_sala, string nome, float pooling, string[] keyUsers)
         {
             this.id = id;
             this.id_sala = id_sala;
             this.nome = nome;
             this.estado = Estado.Undefined;
             this.pooling = pooling;
+            this.keyUser = ToKeyUser(keyUsers);
         }
 
-        public User(string id, string nome, PermissionsUsers permissions)
+        public User(string id, string nome, PermissionsUsers permissions, string[] keyUsers)
         {
             this.id = id;
             this.id_sala = null;
@@ -75,51 +83,81 @@ namespace ControleRemoto
             this.estado = Estado.Undefined;
             this.pooling = 0;
             this.permissions = permissions;
+            this.keyUser = ToKeyUser(keyUsers);
         }
 
-        public bool TextoValido(string texto)
+        //verifica se existe a chave 
+        public bool CheckId(string[] key, string value)
         {
-            return Regex.IsMatch(texto, @"^[a-zA-Z0-9 _-]+$");
+            foreach (var word in key)
+            {
+                if(value == word)
+                { 
+                    return true;
+                }
+            }
+            return false;
         }
 
+        //cria a chave
         public string ToKeyUser(string[] keys)
         {
             RA = id;
             name = nome;
             authorityLevel = permissions;
             
-            this.id = "010101";
-            this.nome = "Higor";
-            this.permissions = PermissionsUsers.FullAccess;
+            string[] divideId = FormateId(id);
+            string tempKey = divideId[0] + nome + divideId[1];
             
+            bool check = CheckId(keys, tempKey);
+            if (!check)
+            {
+                return tempKey;
+            }
+            else
+            {
+                tempKey += "-" + Ramdon(4);
+                
+                return tempKey;
+            }
+        }
 
-            
+        //divide a string pela metade e retorna um vetor com cada metade
+        private string[] FormateId(string id)
+        {
+            char[] vetId = id.ToCharArray();
+            int index = 0;
 
             string KeyUser = "";
 
-            for (int i = 0; i < Length; i++)
-            {
-                if (TextoValido(id))
-                {
-                    KeyUser += id[Random.Range(0, id.Length)];
-                }
-                else
-                {
-                    // fazer a biblioteca para substituição do caractere invalido por um caractere pre-definido
-                }
+            string[] vt = new string[2];
 
-                if (TextoValido(nome))
-                {
-                    KeyUser += nome[Random.Range(0, nome.Length)];
-                }
-                else
-                {
-                    // fazer a biblioteca para substituição do caractere invalido por um caractere pre-definido
-                }
+            for (index = 0; index < (vetId.Length / 2); index++)
+            {
+                vt[0] += vetId[index];
             }
-            keyUser = KeyUser + permissions.ToString();
-            Debug.Log(keyUser);
-            return "null";
+
+            for (index = index; index < vetId.Length; index++)
+            {
+                vt[1] += vetId[index];
+            }
+            return vt;
         }
+
+        //cria uma sequencia aleatoria caso ja exista uma chave
+        private string Ramdon( int range)
+        {
+            string rChars = "";
+
+            for (int i = 0; i < range; i++)
+            {
+                int rIndex = r.NextInt(aleatorio.Length);
+                rChars += aleatorio[rIndex].ToString();
+            }
+            
+
+            return rChars;
+        }
+        
     }
 }
